5f14ef20606601537711cd1078149190d27e4292
Ahnaf Rahat <ahnaf.rahat19@gmail.com>
2021-03-06 02:46:56 +0600

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+<div class="question-box-container">
+    <b-nav>
+    <b-nav-item disabled><b>Quiz App </b>≈</b-nav-item>
+    <b-nav-item disabled>Counter 4/10</b-nav-item>
+    </b-nav>
+</div>
+</template>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <b-jumbotron
+    
+      lead="Bootstrap v4 Components for Vue.js 2"
+    >
+      <template slot="lead"> 
+          {{ currentQuestion.question }}
+         </template>
+
+      <hr class="my-4" />
+
+      <p>List of answer</p>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success" @click="next" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>    
+
+<script>
+export default {
+    props: {
+        currentQuestion: Object,
+        next: Function
+    },
+    mounted: {
+        
+    }
+}
+</script>
